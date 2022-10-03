4007ac9e8604d1d79f99a36df2048282a03c88eb
Emerson <emerson.day@gmail.com>
2022-10-03 14:21:11 -0700

    updated mvc

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import {json} from 'body-parser';
 import todoRoutes from './routes/todos';
+
 const app = express()
+
+app.use(json())
+
 app.use('/todos', todoRoutes);
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
